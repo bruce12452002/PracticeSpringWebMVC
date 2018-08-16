bdd2705af6d97aaea01b5f9f3fe3ce4a9cef75fb
bruce <bruce12452002@yahoo.com.tw>
2018-08-17 00:08:09 +0800

    delete bin

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,13 +1,26 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import bean.Animal;
+import bean.Bird;
+
 @Controller
+@ComponentScan("bean")
 public class MyController {
+	@Autowired
+	private Animal a;
+
+	@Autowired
+	private Bird b;
 
 	@RequestMapping("ooo")
 	public String xxx() {
+		System.out.println(a);
+		System.out.println(b);
 		return "/WEB-INF/success.html";
 	}
 }
